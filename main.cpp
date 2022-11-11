ee7ce429de33364e06161ab3142983799368fdbc
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-11-11 01:05:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,3 +8,17 @@ int main()
 cout << length(&pointP) << endl; 
   return 0;
 }
+
+int main() {
+    Coord3D pointP = {10, 20, 30};
+    Coord3D pointQ = {-20, 21, -22};
+
+    cout << "Address of P: " << &pointP << endl;
+    cout << "Address of Q: " << &pointQ << endl << endl;
+
+    Coord3D * ans = fartherFromOrigin(&pointP, &pointQ);
+   
+    cout << "ans = " << ans << endl; 
+  return 0;
+}
+
