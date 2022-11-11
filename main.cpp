dc9c77ec2a996659e89ba9b6596c48ad206e667a
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-11-11 01:35:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,3 +41,25 @@ cout << pos.x << " " << pos.y << " " << pos.z << endl;
 
 return 0;
 }
+
+
+// task e
+
+int main()
+{
+double x, y, z;
+cout<<"Enter position: ";
+cin>>x>>y>>z;
+  
+Coord3D *ppos = createCoord3D(x, y, z);
+cout<<"\nEnter Velocity:";
+cin>>x>>y>>z;
+  
+Coord3D *pvel = createCoord3D(x, y, z);
+move(ppos, pvel, 10.0);
+cout<<"Coordinates after 10 seconds: "<<(*ppos).x<<" "<<(*ppos).y<<" "<<(*ppos).z<<endl;
+  
+deleteCoord3D(ppos);
+deleteCoord3D(pvel);
+return 0;
+}
