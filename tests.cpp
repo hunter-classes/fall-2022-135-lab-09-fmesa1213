8909b7035197269488dbab1162e2356f8359f557
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-11-11 01:50:56 -0500

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,34 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
-#include "coord3d.h"
-// add your tests here
+#include "coord3.h"
+#include "funcs.cpp"
+
+
+TEST_METHOD(Task_A)
+{
+  double x = 10;
+  double y = 20;
+  double z = 30
+}
+
+TEST_METHOD(Task_B)
+{
+    double x = -20;
+  double y = 21;
+  double z = -22;
+}
+
+TEST_METHOD(Task_C)
+{
+ double x = 5;
+  double y = -3;
+  double z = 1;   
+}
+
+TEST_METHOD(Task_E)
+{
+    double x = 0;
+  double y = 0;
+  double z = 100.0;
+}
