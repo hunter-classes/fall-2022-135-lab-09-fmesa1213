3c694b5e75e1214ffa4f29c83c3f196ee3347134
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-11-11 00:48:16 -0500

    Create 3d-space.cpp

---
+++ 3d-space.cpp
@@ -0,0 +1,35 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Mike Zamansky
+Assignment: Lab 7 B
+Here, I wrote a program in which you define a function length() that receives the coordinates of a point P passed as a pointer, 
+and computes the distance from the origin to the point P
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+struct Coord3D {
+
+double x;
+double y;
+double z;
+
+};
+
+double length(Coord3D *p)
+{
+double len = sqrt(pow(p->x,2) + pow(p->y,2) + pow(p->z,2)); 
+return len;
+}
+
+
+int main() 
+{
+Coord3D pointP = {10, 20, 30};
+cout << length(&pointP) << endl; 
+  return 0;
+}
+
